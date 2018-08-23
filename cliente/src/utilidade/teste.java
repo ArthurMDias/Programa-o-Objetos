f3712e622ae5da1077207c041e3f73066c77e49f
ArthurMDias <37332813+ArthurMDias@users.noreply.github.com>
2018-08-23 17:07:51 -0300

    Add files via upload

--- cliente/src/utilidade/teste.java
+++ cliente/src/utilidade/teste.java
@@ -2,16 +2,41 @@
 package utilidade;
 
 import modelo.Cliente;
+import modelo.Rota;
+import java.util.ArrayList;
 
 public class teste {
-    public static void main(String args[]){
-        Cliente cliente1= new Cliente(); 
+    public static void main(String args[]){  //para ser executado
+        Cliente c1=new Cliente();
         
-        cliente1.nome="Maria Joaquina";
-        cliente1.rua="Rua Duque de Caxias";
-        cliente1.numero=825;
-        cliente1.cpf="999.999.555-55";
+        c1.setNome("Wagner");
+        c1.setRua("Rua Floriano peixoto");
+        c1.setNumero(40);
+        c1.setCpf("99999");
         
-        System.out.println(cliente1.apresentacao());
+        Cliente c2=new Cliente("035.943.090-29");
+        
+        
+        Cliente c3=new Cliente("Arthur", "99999");
+        
+        
+        ArrayList<Cliente> clientes= new ArrayList<Cliente>();
+        
+        clientes.add(c1);
+        clientes.add(c2);
+        clientes.add(c3);
+        
+        Rota rota=new Rota();
+        rota.setNome("Centro/Camobi");
+        rota.setPontosEntrega(clientes); 
+        imprimeRota(rota);
     }
+         private static void imprimeRota(Rota rota){
+         
+         for(int x=0;x<rota.getPontosEntrega().size();x++){
+             Cliente c=rota.getPontosEntrega().get(x);
+             System.out.println(c.getNome());
+         }
+            
+        }
 }
