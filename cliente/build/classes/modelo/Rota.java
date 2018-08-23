e0b9c6c5a5212126f61049dcd98105adbf7bae95
ArthurMDias <37332813+ArthurMDias@users.noreply.github.com>
2018-08-23 17:08:54 -0300

    Add files via upload

--- cliente/build/classes/modelo/Rota.java
+++ cliente/build/classes/modelo/Rota.java
@@ -3,8 +3,28 @@ package modelo;
 import java.util.ArrayList;
 
 public class Rota {
-    public String nome;
-    public ArrayList pontosEntrega=new ArrayList();
+    private String nome;
+    private ArrayList<Cliente> pontosEntrega; //dentro do meu arraylist só vai ter obejetos da classe "Cliente"
     
+    //
+    //
     
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    //
+    //
+           
+    public void setPontosEntrega(ArrayList<Cliente> clientes){
+        pontosEntrega=clientes;
+    }
+    
+    public ArrayList<Cliente> getPontosEntrega(){
+        return pontosEntrega;
+    }
 }
